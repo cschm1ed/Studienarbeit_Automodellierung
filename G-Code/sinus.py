e0025c4c2bdfd384794ef93079d22b9a69b73514
cschm1ed <christian.schmiedt4@gmail.com>
2026-01-31 12:24:42 +0100

    Sinus.py angepasst, sodass der generierte G-Code mit Estlcam funktioniert

--- G-Code/sinus.py
+++ G-Code/sinus.py
@@ -9,7 +9,7 @@ dt = 0.01
 T  = 5.0
 
 # optional: Offset, damit du nicht um 0 herum fährst (falls du das nicht willst)
-x_offset = -500  # mm
+x_offset = 500  # mm
 
 # optional: Begrenzung Nachkommastellen für CNC
 decimals = 3
@@ -20,7 +20,7 @@ x = (A1*np.sin(2*np.pi*f1*t) + A2*np.sin(2*np.pi*f2*t)) + x_offset
 
 # --- Geschwindigkeit -> Vorschubabschätzung ---
 v = np.gradient(x, dt)           # mm/s
-F_required = np.max(np.abs(v)) * 60.0  # mm/min
+F_required = np.max(np.abs(v)) * 50.0  # mm/min
 
 # Sicherheitsfaktor (z.B. 10% Reserve)
 F_set = 1.1 * F_required
@@ -30,20 +30,19 @@ print(f"Empfohlener G-Code Vorschub F ~ {F_set:.1f} mm/min")
 # --- G-Code schreiben ---
 outfile = "doppelsinus.nc"
 with open(outfile, "w", encoding="utf-8") as f:
-    f.write("%\n")
-    f.write("G90\n")          # Absolut
-    f.write("G21\n")          # mm
-    f.write(f"G1 F{F_set:.1f}\n")
     f.write("\n")
 
     # optional: Start anfahren
     f.write(f"G1 X{round(x[0], decimals):.{decimals}f}\n")
 
+
+
+    f.write("M09\n")
+
     # Trajektorie
     for xi in x[1:]:
-        f.write(f"G1 X{round(xi, decimals):.{decimals}f}\n")
+        f.write(f"G1 X{round(xi, decimals):.{decimals}f} F{round(F_set)}\n")
 
-    f.write("\nM30\n")
-    f.write("%\n")
+    f.write("M08\n")
 
 print(f"G-Code gespeichert: {outfile}")
